82ab701414296c5bdf59f99c443c888ddba91d58
Jonnhymoraes <moraesjonnhy@gmail.com>
2021-03-10 20:25:45 -0300

    array methods

--- Projeto/Aulas JS/metodos_array.js
+++ Projeto/Aulas JS/metodos_array.js
@@ -1,12 +1,11 @@
-const students = [
-    {id: 1, name: 'John Doe', age: 23, scholarship: false},
-    {id: 2, name: 'Margoe Rose', age: 19, scholarship: true},
-    {id: 3, name: 'Kayle', age: 22, scholarship: true}
-]
+/* //MÉTODOS DE ARRAY
 
-const result = students.map((student, indice) => {
-  return indice + " " + student.name;
-});
+MAP -> cria um novo array modificando ou mapeando os elementos do array
+FIND -> busca o PRIMEIRO elemento do array que satisfaça a condição informada
+SOME -> informa se ALGUM elemento do array satisfaz a condição informada
+EVERY -> informa se TODOS elemento do array satisfaz a condição informada
+FILTER -> cria um novo array contendo somente os elementos que satisfaçam a condição informada
+REDUNCE -> reduz o array à partir da função passada como argumento e retorna o valor acumulado nesse processo
+SORT -> muda a posição dos elementos do array para ordenar de acordo com a função de ordenação passada como argumento 
 
-console.log(result);
-console.log(students);
+*/
