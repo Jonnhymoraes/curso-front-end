9044c714b7c9866c513d47420bd02aa97fbceb3f
Jonnhymoraes <moraesjonnhy@gmail.com>
2021-03-12 09:42:09 -0300

    challange of specific methods

---
+++ Projeto/Aulas JS/Desafio Métodos Específicos/exerc_4.js
@@ -0,0 +1,38 @@
+const colaboradores = [
+    {
+        name: 'Juca',
+        idade: 25,
+        cargo: 'front-end'
+    },
+    {
+        name: 'Márcia',
+        idade: 23,
+        cargo: 'back-end'
+    },
+    {
+        name: 'Vitória',
+        idade: 28,
+        cargo: 'designer'
+    },
+    {
+        name: 'Fernando',
+        idade: 19,
+        cargo: 'estagiario'
+    },
+    {
+        name: 'Fabiane',
+        idade: 25,
+        cargo: 'back-end'
+    },
+    {
+        name: 'Jéssica',
+        idade: 23,
+        cargo: 'front-end'
+    },
+  ]
+
+  const resulto = colaboradores.every(colaborador => {
+  return colaborador.idade > 18;
+})
+
+console.log("Resposta:", resulto);
