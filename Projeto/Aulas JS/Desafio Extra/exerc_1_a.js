f280e4ebebb882426e913a7e8771e82a7063f323
Jonnhymoraes <moraesjonnhy@gmail.com>
2021-03-14 19:08:28 -0300

    challenge extra

---
+++ Projeto/Aulas JS/Desafio Extra/exerc_1_a.js
@@ -0,0 +1,32 @@
+const installments = [
+  {
+    installment_number: 1,
+    value: 123.45,
+    status: 'Pago'
+  },
+  {
+    installment_number: 2,
+    value: 139.88,
+    status: 'Pago'
+  },
+  {
+    installment_number: 3,
+    value: 123.45,
+    status: 'Pago'
+  },
+  {
+    installment_number: 4,
+    value: 182.37,
+    status: 'Aberto'
+  },
+  {
+    installment_number: 5,
+    value: 133.93,
+    status: 'Aberto'
+  }
+]
+
+const result = installments.reduce((acc, parcelas) => {
+return acc + parcelas.value;
+}, 0);
+console.log("Total das Parcelas: ", result.toFixed(2));
